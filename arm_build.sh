e2c3f72cd5b0be0f0ccf3195af55d0bb75c7818d
Maksim Leiman <maximleyman@yandex.ru>
2026-05-13 14:23:34 +0300

    fix build scripts

--- arm_build.sh
+++ arm_build.sh
@@ -11,11 +11,11 @@ CURRENT_DIR="$(pwd)";
 TARGET=armv7-unknown-linux-gnueabihf
 
 aurora_psdk="$PSDK_DIR/sdk-chroot"
-PKG_VERSION="$(cargo pkgid | cut -d \# -f 2)"
+PKG_VERSION="$(cargo pkgid | cut -d @ -f 2)"
 
 mkdir -p RPMS/
 
 cross build --release --target $TARGET
 cargo generate-rpm -a armv7hl --target $TARGET -o RPMS/
 
-$aurora_psdk rpmsign-external sign -k $PSDK_DIR/../../certs/regular_key.pem -c $PSDK_DIR/../../certs/regular_cert.pem "$CURRENT_DIR/RPMS/com.lmaxyz.egui_aurora_app-$PKG_VERSION-1.armv7hl.rpm"
+$aurora_psdk rpmsign-external sign -k $PSDK_DIR/../../certs/lmaxyz_key.pem -c $PSDK_DIR/../../certs/lmaxyz_cert.pem "$CURRENT_DIR/RPMS/com.lmaxyz.egui_aurora_app-$PKG_VERSION-1.armv7hl.rpm"
